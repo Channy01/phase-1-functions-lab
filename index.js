20ff518cb53905572cae8631694eaecf2d710586
ChantiaKindle <chantia.kindle0123@gmail.com>
2022-03-27 22:21:35 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  const hqStreet = 42;
+  if (streetNumber > hqStreet) {
+    return streetNumber - hqStreet;
+  } else {
+    return hqStreet - streetNumber;
+  }
+}
+// returns the distance from the HQ street to its destination street in feet
+function distanceFromHqInFeet(streetNumber) {
+  const oneBlock = 264;
+  return distanceFromHqInBlocks(streetNumber) * oneBlock;
+}
+// Calculates the number of feet using the given starting block and an ending block — it only calculates distance North and South (uptown/downtown).
+function distanceTravelledInFeet(startBlock, arriveBlock) {
+  const oneBlock = 264;
+  return Math.abs(startBlock - arriveBlock) * oneBlock;
+}
+/* GIVEN NOTES FOR THE 4TH/FINAL FUNCTION CHALLENGE BELOW:
+    - First 400 ft are free.
+    - Distance between 400 - 2000 ft, the price is 2 cents/ft (0.02)
+    - Flat fare ($25) over 2000 ft and under 2500 ft
+    - Any distance over 2500 will return a message/string: "Cannot travel that far."
+ */
+
+function calculatesFarePrice(startBlock, arriveBlock) {
+  const myRide = distanceTravelledInFeet(startBlock, arriveBlock);
+  const flatFare = 25;
+  if (myRide > 400 && myRide <= 2000) {
+    return (myRide - 400) * 0.02;
+  } else if (myRide > 2000 && myRide <= 2500) {
+    return flatFare;
+  } else if (myRide > 2500) {
+    return "cannot travel that far";
+  } else {
+    return 0;
+  }
+}
